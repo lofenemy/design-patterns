6a37769e8696599611aaa0cd5eeadc7eef2a0829
Lofe <bebebelofe@gmail.com>
2020-07-08 21:01:47 +0300

    Finished Singleton.

--- singleton.js
+++ singleton.js
@@ -1,1 +1,11 @@
-class Singleton {}
+class Singleton {
+  constructor() {
+    if (!Singleton.instance) {
+      Singleton.instance = this;
+    }
+
+    return Singleton.instance;
+  }
+}
+
+// use static value of class to save instance. We can also use external variable.`
